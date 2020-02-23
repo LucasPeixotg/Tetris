5f8ea7c31c65e21ee2bc72d2d7e02e31bc826dc6
Lucas Peixoto Gonçalves <Lucaspeixotopro@gmail.com>
2020-02-23 17:28:02 -0300

    main menu added.

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ import random
 import pygame
 from shapes import shapes, shapes_colors
 
-block_size = 20
-screen_width = 20
-screen_height = 30
+block_size = 22
+screen_width = 22
+screen_height = 33
 game_FPS = 10
 
 
 pygame.init()
 screen = pygame.display.set_mode((screen_width*block_size, screen_height*block_size))
+
+font1 = pygame.font.Font('roboto.ttf', 65)
+font2 = pygame.font.Font('roboto.ttf', 25)
+tetris_text = font1.render("Tetris", True, (255, 255, 255))
+press_text = font2.render("Press SPACE to play", True, (200, 200, 200))
+
 pygame.display.set_caption('Tetris') 
 
 class Piece:
@@ -64,7 +70,7 @@ def valid_move(current_piece_coordinates, grid):
 
     return valid
 
-def lock_check(current_piece_coordinates, grid):
+def lock_check(current_piece_coordinates, grid, score):
     locked = False
 
 
@@ -76,7 +82,7 @@ def lock_check(current_piece_coordinates, grid):
                 locked = True
 
         if locked and y == 0:
-            game_over()
+            game_over(score=score)
 
     return locked
 
@@ -104,9 +110,9 @@ def delete_rows(score, locked_positions=[]):
 
     return locked_positions, score
 
-def game_over():
+def game_over(score):
     pygame.time.delay(2000//game_FPS)
-    main()
+    main_menu(score)
 
 
 '''
@@ -125,13 +131,12 @@ def draw_grid(surface, grid, current_piece_color):
 
 
 def main():
-    run = True
     current_piece = Piece(screen_width//2, -4, random.choice(shapes))
     locked_positions = []
     grid = update_grids()
     score = 0
 
-    while run:
+    while True:
         # EVENTS
         
         pygame.time.delay(1000//game_FPS)
@@ -163,8 +168,8 @@ def main():
                     main()
 
         # GAME SCREEN
-        pygame.draw.rect(screen, (30, 32, 33), (0,0,screen_width*block_size, screen_height*block_size))
-        pygame.draw.rect(screen, (255, 50, 50, 12), (0, 0, screen_width*block_size, block_size))
+        screen.fill((30, 32, 33))
+        pygame.draw.rect(screen, (120, 50, 50), (0, 0, screen_width*block_size, block_size))
         draw_grid(screen, grid, current_piece.color)
 
         pygame.display.update()
@@ -172,7 +177,7 @@ def main():
         # GAME LOGIC
         current_piece.y += 1
 
-        if lock_check(current_piece.coordinates(), grid):
+        if lock_check(current_piece.coordinates(), grid, score=score):
             current_piece.y -= 1
             for coordinate in current_piece.coordinates():
                 locked_positions.append(coordinate)
@@ -186,8 +191,36 @@ def main():
             current_piece_coordinates=current_piece.coordinates()
         )
         
-def main_menu(score):
-    return
+def main_menu(score=None):
+    show_text = True
+    score_text_title = font2.render("score:", True, (200, 200, 200))
+    score_text_quant = font1.render(str(score), True, (215, 215, 215))
+
+    while True:
+        # EVENTS
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    main()
+
+        screen.fill((30, 32, 33))
+        screen.blit(tetris_text, (screen_width*block_size//2 - tetris_text.get_width() // 2, round(screen_height*block_size//2 - tetris_text.get_height() // 2) - 6 * block_size))
+        if show_text:
+            screen.blit(press_text, (screen_width*block_size//2 - press_text.get_width() // 2, round(screen_height*block_size//2 - press_text.get_height() // 2) - 2 * block_size))
+        
+        if score != None:
+            screen.blit(score_text_title, (screen_width*block_size//2 - score_text_title.get_width() // 2, round((screen_height*block_size//2 - score_text_title.get_height() // 2)) + 1.5 * block_size))
+            screen.blit(score_text_quant, (screen_width*block_size//2 - score_text_quant.get_width() // 2, round((screen_height*block_size//2 - score_text_quant.get_height() // 2)) + 3 * block_size))
 
 
-main()
+
+        pygame.display.update()
+
+        pygame.time.delay(500)
+        show_text = not show_text
+
+
+main_menu()
